2b46eda76d58416604dad0e261d558d643dec2a6
Mico D. Ang <mico.dahang@gmail.com>
2021-05-06 08:21:19 +0800

    [update] added InputField component

---
+++ src/components/base/InputField.vue
@@ -0,0 +1,30 @@
+<template>
+	<b-form-group
+		:id="`input-group-${data.id}`"
+		:label="data.field"
+		:label-for="data.id"
+	>
+		<b-form-input
+			:id="data.id"
+			v-model="data.value"
+			type="email"
+			:placeholder="data.placeholder"
+			required
+		></b-form-input>
+	</b-form-group>
+</template>
+
+<script>
+export default {
+	props: {
+		data: {
+			default: () => {
+				return {};
+			},
+			type: Object,
+		},
+	},
+};
+</script>
+
+<style></style>
