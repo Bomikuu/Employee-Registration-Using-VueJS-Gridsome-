e20a9d50c5a8e37c1f7eb37cf5fab069ddae38e3
Mico D. Ang <mico.dahang@gmail.com>
2021-05-06 11:52:41 +0800

    [update] added API for company

--- src/api/employee.js
+++ src/api/employee.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const apiUrl = process.env.GRIDSOME_API_URL + "/employees";
+const companyUrl = process.env.GRIDSOME_API_URL + "/company";
 
 function getAllEmployees() {
 	return axios.get(apiUrl);
@@ -18,9 +19,14 @@ function saveEmployee(payload) {
 	return axios.post(apiUrl, payload);
 }
 
+function saveCompany(payload) {
+	return axios.post(companyUrl, payload);
+}
+
 export default {
 	getAllEmployees,
 	getEmployeeByEmail,
 	updateEmployee,
 	saveEmployee,
+	saveCompany,
 };
