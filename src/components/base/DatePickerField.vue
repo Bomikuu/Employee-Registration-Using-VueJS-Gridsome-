f3687ba58d3457d562b46bd1bc635ffdf817c13d
Mico D. Ang <mico.dahang@gmail.com>
2021-05-06 08:21:06 +0800

    [update] added DatePicker component

---
+++ src/components/base/DatePickerField.vue
@@ -0,0 +1,29 @@
+<template>
+	<b-form-group
+		:id="`input-group-${data.id}`"
+		:label="data.field"
+		:label-for="data.id"
+	>
+		<b-form-datepicker
+			:id="data.id"
+			v-model="data.value"
+			class="mb-2"
+			:placeholder="data.placeholder"
+		></b-form-datepicker>
+	</b-form-group>
+</template>
+
+<script>
+export default {
+	props: {
+		data: {
+			default: () => {
+				return {};
+			},
+			type: Object,
+		},
+	},
+};
+</script>
+
+<style></style>
